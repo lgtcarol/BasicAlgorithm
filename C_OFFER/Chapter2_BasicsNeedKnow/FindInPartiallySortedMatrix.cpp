2e70fa99f8660782a8a5634f413a699e5c0a04ad
lgtcarol <lgtcarol@gmail.com>
2018-11-07 14:42:14 +0800

    Add files via upload
    4：二维数组中的查找

---
+++ C_OFFER/Chapter2_BasicsNeedKnow/FindInPartiallySortedMatrix.cpp
@@ -0,0 +1,120 @@
+#include <cstdio>
+//本题还有个收获就是：二维数组用一维指针变量然后加上偏移就轻松访问 
+bool Find(int *matrix, int rows, int columns, int number)
+{
+	bool found = false;
+	int realr = 0, realc = columns-1;
+	if(matrix == NULL || rows <= 0 || columns <= 0)
+		return found;
+
+	while(realr < rows && realc >= 0)
+	{
+		if(matrix[realr*columns + realc] == number)
+		{
+			found = true;
+			break;//找到则退出循环！ 
+		}
+		else if(matrix[realr*columns + realc] < number)
+			realr++;
+		else 
+			realc--;
+	}
+	return found;
+} 
+// ====================测试代码====================
+void Test(const char* testName, int* matrix, int rows, int columns, int number, bool expected)
+{
+    if(testName != NULL)
+        printf("%s begins: ", testName);
+
+    bool result = Find(matrix, rows, columns, number);
+    if(result == expected)
+        printf("Passed.\n");
+    else
+        printf("Failed.\n");
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数在数组中
+void Test1()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test1", (int*)matrix, 4, 4, 7, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数不在数组中
+void Test2()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test2", (int*)matrix, 4, 4, 5, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最小的数字
+void Test3()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test3", (int*)matrix, 4, 4, 1, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最大的数字
+void Test4()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test4", (int*)matrix, 4, 4, 15, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最小的数字还小
+void Test5()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test5", (int*)matrix, 4, 4, 0, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最大的数字还大
+void Test6()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test6", (int*)matrix, 4, 4, 16, false);
+}
+
+// 鲁棒性测试，输入空指针
+void Test7()
+{
+    Test("Test7", NULL, 0, 0, 16, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+    return 0;
+}

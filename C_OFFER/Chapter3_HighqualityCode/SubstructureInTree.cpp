0ca6e3efa24d9377c2518c029f4b67e96c1cdce2
lgtcarol <lgtcarol@gmail.com>
2018-11-16 19:53:48 -0600

    Add files via upload

---
+++ C_OFFER/Chapter3_HighqualityCode/SubstructureInTree.cpp
@@ -0,0 +1,371 @@
+// 面试题26：树的子结构
+// 题目：输入两棵二叉树A和B，判断B是不是A的子结构。
+
+#include <cstdio>
+#include <cmath>
+
+struct BinaryTreeNode
+{
+	double  m_dbValue;
+	BinaryTreeNode *m_pLeft;
+	BinaryTreeNode *m_pRight;
+};
+bool Equal(double n1, double n2)
+{
+	if(fabs(n1-n2) < 1e-7)
+		return true;
+	else
+		return false;
+}
+bool DoseTree1haveTree2(BinaryTreeNode *r1, BinaryTreeNode *r2) 
+{
+	/*
+	1. 你并不能保证这两棵树就会匹配，所以判断空节点不能丢（链结务必判断结尾）
+	2. 但判空和判结束就要区分了 
+	2. hasSubTree其实是或关系的递归结构，此处是与关系的递归结构 
+	*/
+	if(r2 == NULL)
+		return true;
+	if(r1 == NULL)
+		return false;
+	//一个点不满足则全不满足 
+	if(!Equal(r1->m_dbValue, r2->m_dbValue))
+		return false;
+	return DoseTree1haveTree2(r1->m_pLeft,r2->m_pLeft)&&DoseTree1haveTree2(r1->m_pRight,r2->m_pRight);
+	
+}
+
+bool HasSubtree(BinaryTreeNode *r1, BinaryTreeNode *r2)
+{
+	bool has = false; 
+	if(r1 != NULL &&  r2 != NULL)
+	{
+		if(Equal(r1->m_dbValue, r2->m_dbValue))
+		{
+			has = DoseTree1haveTree2(r1,r2);
+		}
+		if(has != true)
+			has = HasSubtree(r1->m_pLeft, r2);
+		if(has != true)
+			has = HasSubtree(r1->m_pRight, r2);
+	}
+		return has;	
+}
+// ====================辅助测试代码====================
+BinaryTreeNode* CreateBinaryTreeNode(double dbValue)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_dbValue = dbValue;
+    pNode->m_pLeft = NULL;
+    pNode->m_pRight = NULL;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != NULL)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != NULL)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = NULL;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
+
+// ====================测试代码====================
+void Test(const char* testName, BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2, bool expected)
+{
+    if(HasSubtree(pRoot1, pRoot2) == expected)
+        printf("%s passed.\n", testName);
+    else
+        printf("%s failed.\n", testName);
+}
+
+// 树中结点含有分叉，树B是树A的子结构
+//                  8                8
+//              /       \           / \
+//             8         7         9   2
+//           /   \
+//          9     2
+//               / \
+//              4   7
+void Test1()
+{
+    BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA2 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA3 = CreateBinaryTreeNode(7);
+    BinaryTreeNode* pNodeA4 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeA5 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNodeA6 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNodeA7 = CreateBinaryTreeNode(7);
+
+    ConnectTreeNodes(pNodeA1, pNodeA2, pNodeA3);
+    ConnectTreeNodes(pNodeA2, pNodeA4, pNodeA5);
+    ConnectTreeNodes(pNodeA5, pNodeA6, pNodeA7);
+
+    BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeB2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeB3 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNodeB1, pNodeB2, pNodeB3);
+
+    Test("Test1", pNodeA1, pNodeB1, true);
+
+    DestroyTree(pNodeA1);
+    DestroyTree(pNodeB1);
+}
+
+// 树中结点含有分叉，树B不是树A的子结构
+//                  8                8
+//              /       \           / \
+//             8         7         9   2
+//           /   \
+//          9     3
+//               / \
+//              4   7
+void Test2()
+{
+    BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA2 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA3 = CreateBinaryTreeNode(7);
+    BinaryTreeNode* pNodeA4 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeA5 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNodeA6 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNodeA7 = CreateBinaryTreeNode(7);
+
+    ConnectTreeNodes(pNodeA1, pNodeA2, pNodeA3);
+    ConnectTreeNodes(pNodeA2, pNodeA4, pNodeA5);
+    ConnectTreeNodes(pNodeA5, pNodeA6, pNodeA7);
+
+    BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeB2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeB3 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNodeB1, pNodeB2, pNodeB3);
+
+    Test("Test2", pNodeA1, pNodeB1, false);
+
+    DestroyTree(pNodeA1);
+    DestroyTree(pNodeB1);
+}
+
+// 树中结点只有左子结点，树B是树A的子结构
+//                8                  8
+//              /                   / 
+//             8                   9   
+//           /                    /
+//          9                    2
+//         /      
+//        2        
+//       /
+//      5
+void Test3()
+{
+    BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA2 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA3 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeA4 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNodeA5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNodeA1, pNodeA2, NULL);
+    ConnectTreeNodes(pNodeA2, pNodeA3, NULL);
+    ConnectTreeNodes(pNodeA3, pNodeA4, NULL);
+    ConnectTreeNodes(pNodeA4, pNodeA5, NULL);
+
+    BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeB2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeB3 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNodeB1, pNodeB2, NULL);
+    ConnectTreeNodes(pNodeB2, pNodeB3, NULL);
+
+    Test("Test3", pNodeA1, pNodeB1, true);
+
+    DestroyTree(pNodeA1);
+    DestroyTree(pNodeB1);
+}
+
+// 树中结点只有左子结点，树B不是树A的子结构
+//                8                  8
+//              /                   / 
+//             8                   9   
+//           /                    /
+//          9                    3
+//         /      
+//        2        
+//       /
+//      5
+void Test4()
+{
+    BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA2 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA3 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeA4 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNodeA5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNodeA1, pNodeA2, NULL);
+    ConnectTreeNodes(pNodeA2, pNodeA3, NULL);
+    ConnectTreeNodes(pNodeA3, pNodeA4, NULL);
+    ConnectTreeNodes(pNodeA4, pNodeA5, NULL);
+
+    BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeB2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeB3 = CreateBinaryTreeNode(3);
+
+    ConnectTreeNodes(pNodeB1, pNodeB2, NULL);
+    ConnectTreeNodes(pNodeB2, pNodeB3, NULL);
+
+    Test("Test4", pNodeA1, pNodeB1, false);
+
+    DestroyTree(pNodeA1);
+    DestroyTree(pNodeB1);
+}
+
+// 树中结点只有右子结点，树B是树A的子结构
+//       8                   8
+//        \                   \ 
+//         8                   9   
+//          \                   \
+//           9                   2
+//            \      
+//             2        
+//              \
+//               5
+void Test5()
+{
+    BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA2 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA3 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeA4 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNodeA5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNodeA1, NULL, pNodeA2);
+    ConnectTreeNodes(pNodeA2, NULL, pNodeA3);
+    ConnectTreeNodes(pNodeA3, NULL, pNodeA4);
+    ConnectTreeNodes(pNodeA4, NULL, pNodeA5);
+
+    BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeB2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeB3 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNodeB1, NULL, pNodeB2);
+    ConnectTreeNodes(pNodeB2, NULL, pNodeB3);
+
+    Test("Test5", pNodeA1, pNodeB1, true);
+
+    DestroyTree(pNodeA1);
+    DestroyTree(pNodeB1);
+}
+
+// 树A中结点只有右子结点，树B不是树A的子结构
+//       8                   8
+//        \                   \ 
+//         8                   9   
+//          \                 / \
+//           9               3   2
+//            \      
+//             2        
+//              \
+//               5
+void Test6()
+{
+    BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA2 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA3 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeA4 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNodeA5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNodeA1, NULL, pNodeA2);
+    ConnectTreeNodes(pNodeA2, NULL, pNodeA3);
+    ConnectTreeNodes(pNodeA3, NULL, pNodeA4);
+    ConnectTreeNodes(pNodeA4, NULL, pNodeA5);
+
+    BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeB2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeB3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNodeB4 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNodeB1, NULL, pNodeB2);
+    ConnectTreeNodes(pNodeB2, pNodeB3, pNodeB4);
+
+    Test("Test6", pNodeA1, pNodeB1, false);
+
+    DestroyTree(pNodeA1);
+    DestroyTree(pNodeB1);
+}
+
+// 树A为空树
+void Test7()
+{
+    BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeB2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeB3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNodeB4 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNodeB1, NULL, pNodeB2);
+    ConnectTreeNodes(pNodeB2, pNodeB3, pNodeB4);
+
+    Test("Test7", NULL, pNodeB1, false);
+
+    DestroyTree(pNodeB1);
+}
+
+// 树B为空树
+void Test8()
+{
+    BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNodeA2 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNodeA3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNodeA4 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNodeA1, NULL, pNodeA2);
+    ConnectTreeNodes(pNodeA2, pNodeA3, pNodeA4);
+
+    Test("Test8", pNodeA1, NULL, false);
+
+    DestroyTree(pNodeA1);
+}
+
+// 树A和树B都为空
+void Test9()
+{
+    Test("Test9", NULL, NULL, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
+    Test9();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+

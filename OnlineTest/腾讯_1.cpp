48f0a91797b477cdf863d6e0d6979a423dc954f8
lgtcarol <lgtcarol@gmail.com>
2019-04-10 23:01:08 +0800

    Add files via upload

---
+++ OnlineTest/腾讯_1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//认真理解题意的输出是怎么回事，思路非常简单！
+int main(void)
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr;
+    for(int i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        arr.push_back(t);
+    }
+    sort(arr.begin(), arr.end());
+    int minx = arr[0];
+    cout << minx << endl;
+    for(k-=1; k>0; k--)
+    {
+       for(int i = 0; i < arr.size(); i++)
+       {
+            if(arr[i] != 0)
+                arr[i] -= minx;
+            else
+                continue;
+       }
+       bool flag = false;
+       int i;
+       for(i = 0; i < arr.size(); i++)
+       {
+           if(arr[i] > 0)
+           {
+               flag = true;
+               break;
+           }
+       }
+       if(flag)
+            cout << arr[i] << endl;
+       else
+            cout << 0 << endl;
+    }
+    return 0;
+}

e8de4fd4eb9703f5fecd9f78aa164c77752331fc
lgtcarol <lgtcarol@gmail.com>
2019-04-13 13:52:54 +0800

    Add files via upload

---
+++ OnlineTest/lgt_template.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+
+    for(int i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        arr.push_back(t);
+    }
+    cout << "check over!" << endl;
+
+  //  cout << res << endl;
+    return 0;
+}

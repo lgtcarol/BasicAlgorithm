d593b30703b069d45f618f5a5419ea7db8d9bdbf
lgtcarol <lgtcarol@gmail.com>
2019-05-08 17:35:10 +0800

    Create 二叉搜索树第K小.cpp

---
+++ GramRelated/二叉搜索树第K小.cpp
@@ -0,0 +1,26 @@
+/*
+1. Debug:这是个带有返回值的递归函数。子函数返回值该如何记载并逐层返回为问题的解！
+2. 二叉搜索树的中序遍历，当对root的操作在中间部分时。
+*/
+class Solution {
+    int cnt = 0;
+public:
+    TreeNode* KthNode(TreeNode* pRoot, int k)
+    {
+        TreeNode *res = NULL;
+        if(pRoot == NULL)
+            return res;
+        if(pRoot->left)
+            res = KthNode(pRoot->left, k);
+        if(!res)
+        {
+            cnt++;
+            if(cnt == k)
+                res = pRoot;           
+        }
+
+        if(!res && pRoot->right)
+            res = KthNode(pRoot->right, k);
+        return res;
+    }
+};

5639a7ef16a31116e42e1709a54ed3b2dbc61893
lgtcarol <lgtcarol@gmail.com>
2019-05-24 10:26:25 +0800

    Update LeetcodeArray_排序数组中删元素.cpp

--- Practices/LeetcodeArray_排序数组中删元素.cpp
+++ Practices/LeetcodeArray_排序数组中删元素.cpp
@@ -13,3 +13,29 @@ int removeDuplicates(int A[], int n)
     //由于j指向的当前存好的有效元素，故总长度需要加一（数组下标和长度的关系）
     return j+1；
 }
+
+//变体:若允许出现两次（仍旧有序数组）
+int removeDuplicates(int A[], int n)
+{
+    if(n == 0)
+        return 0;
+    int j = 0;
+    int num = 0;
+    for(i = 1; i < n; i++)
+    {
+        //相比于只允许出现一次，增加一个计数当相等时若次数允许，按照不重复的hli
+        if(A[j] == A[i])
+        {
+            num++;
+            if(num < 2)
+                A[++j] = A[i];
+        }
+        else
+        {
+            //注意此处对新元素计数器的重置
+            A[++j] = A[i];
+            num = 0;
+        }
+    }
+    return j+1;
+}
